f682f35d604633d24449158c67437ebcf10eb714
yacine namane <yacine.nam@outlook.fr>
2023-07-20 15:52:06 +0200

    petites retouches a la partie js pour afficher les article correctement

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ await fetch("http://localhost:5678/api/works")
 const galleryDisplay = async () =>{
     await fetchgellery();
 
-document.getElementById('portfolio').innerHTML = gallery.map((figure)=> `
+document.querySelector(".gallery").innerHTML = gallery.map((figure)=> `
 <div id= "figure${figure.id}" class=".gallery "> 
-<img class=".gallery " src="${figure.imageUrl}" alt=" image gallery " alt ="imagie figure ${figure.name}"/>
-<figcaption>${figure.title}</figcaption>
+<img class= ".gallery img " src="${figure.imageUrl}" alt=" image gallery " alt ="imagie figure ${figure.name}"/>
+<figcaption >${figure.title}</figcaption>
 </div>
 `,
-);
+)
+.join("");
 };
 
 galleryDisplay();
