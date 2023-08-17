5fa89798991f84004800ab410ec274b45b8638e8
yacine namane <yacine.nam@outlook.fr>
2023-08-17 17:24:02 +0200

    mise en place de la fonction ajout d'un work / suppression de la galerie / suppression d'un element de la modal ainsi que la page d'accueil( faut refresh pour que ça se mette a jour sur la page d'accueil atm )

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const galleryDisplay = async (works) => {
   document.querySelector(".gallery ").innerHTML = works
     .map(
       (figure) => `
-    <div id= "figure${figure.id}" class=".gallery "> 
+    <div id= "figure${figure.id}" class=".gallery "data-id="${figure.id}"> 
     <img class= ".gallery img " src="${figure.imageUrl}" alt=" image gallery " alt ="imagie figure ${figure.name}"/>
     <figcaption >${figure.title}</figcaption>
     </div>
@@ -53,30 +53,87 @@ const init = async () => {
 
   // DELETE WORK
 
-  const deleteProject = document.querySelectorAll(".fa-trash-can");
-  let id = 0;
-  for (let i = 0; i < deleteProject.length; i++) {
-    deleteProject[i].addEventListener("click", (e) => {
-      e.preventDefault();
+  const figuresModal = document.querySelectorAll(".figure-modal");
 
-      const workId = gallery.map((figure) => figure.id);
-      id = workId[i];
+  const data = JSON.parse(connectedData);
+
+  figuresModal.forEach((figure) => {
+    const trash = figure.querySelector("i");
+
+    trash.addEventListener("click", (e) => {
+      const id = figure.dataset.id;
+
       fetch(`http://localhost:5678/api/works/${id}`, {
         method: "DELETE",
+
         headers: {
-          Authorization: `Bearer ${connectedData}`,
+          Authorization: `Bearer ${data.token}`,
         },
       })
         .then((response) => {
           if (response.ok) {
+            const currentWork = document.getElementById(`work-modal${id}`);
+
+            const figurePage = document.getElementById(`figure${id}`);
+
+            deleteWorkOnModal(figure);
+            deleteWorkPage(figure);
+
+            // deleteWorkPage(figurePage)
+
             console.log("Deleted");
           } else {
-            console.log("cannot delte this something went wrong!");
+            console.log("cannot delete this something went wrong!");
           }
         })
+
         .catch((error) => console.log(error));
     });
-  }
+  });
+  const deleteWorkOnModal = (work) => {
+    console.log(work);
+
+    const modalContain = document.querySelector(".modal-body");
+
+    modalContain.removeChild(work);
+  };
+
+  const deleteWorkPage = (work) => {
+    const workContain = document.querySelector(".gallery ");
+
+    workContain.removeChild(work);
+  };
+
+  // ADD new work
+  // load image > modal.js
+  const formAjout = document.getElementById("formAjout");
+  formAjout.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const image = document.getElementById("ajoutImage").files[0];
+    console.log(image);
+    console.log(typeof image);
+    const title = document.querySelector(".image-title-form").value;
+    const category = parseInt(document.querySelector(".catégorie"));
+    let formData = new FormData();
+    formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", category);
+    console.log(formData);
+    let response = await fetch(`http://localhost:5678/api/works`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      },
+      body: formData,
+    })
+      .then((response) => {
+        let data = response.json();
+        alert("project sent !");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  });
 };
 
 init();
--- modal.js
+++ modal.js
@@ -9,7 +9,7 @@ const galleryDisplay = async (works) => {
   document.querySelector(".modal-body").innerHTML = works
     .map(
       (figure) => `
-    <div class="figure-modal" id= "figure${figure.id}" > 
+    <div class="figure-modal" id= "figure${figure.id}" data-id="${figure.id}"> 
     <img src="${figure.imageUrl}"  alt=" image gallery " alt ="imagie figure ${figure.name}" />
     <div ><i class="fa-solid fa-trash-can"></i></div>
     <figcaption >${figure.title}</figcaption>
@@ -45,4 +45,25 @@ function openEditBtn() {
 // formAjout
 function openAjoutBtn() {
   document.getElementById("formAjout").style.display = "block";
+}
+
+//delete galerie modal
+function deleteModal() {
+  document.querySelector(".modal-body").style.display = "none";
+}
+
+// load image
+function loadFile(event) {
+  const img = document.getElementById("output");
+  img.src = URL.createObjectURL(event.target.files[0]);
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    img.src = reader.result;
+  });
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+  document.getElementById("hide").style.display = "none";
+  document.getElementById("hide").style.display = "submit";
 }
